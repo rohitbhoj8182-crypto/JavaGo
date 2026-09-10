fc760768f0d6c74580f9110686396c15d13660b9
rohitbhoj8182-crypto <rohitbhoj8182@gmail.com>
2026-09-10 15:56:22 +0530

    First java Program

---
+++ myprogram/src/Main.java
@@ -0,0 +1,11 @@
+public  class Main {
+    static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+
+        int age = 21;
+        System.out.println(age);
+        
+        double price = 11.1;
+        System.out.println("double eaxmple is " +price);
+    }
+}
